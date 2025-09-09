6e89c0259dcdc67b9e19970f94bbbe2553a4a7c0
greatdignity <emaiwoolabipupo@gmail.com>
2025-09-09 10:59:13 +0100

    django_project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -48,3 +48,38 @@ class LibraryDetailView(DetailView):
         # Ensure books are prefetched for performance
         ctx["books"] = self.object.books.select_related("author").all()
         return ctx
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+# Registration view
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after registration
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
