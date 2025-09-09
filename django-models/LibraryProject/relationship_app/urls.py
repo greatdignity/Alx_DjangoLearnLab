31d7660c348c35f41041346974607c3b1473e001
greatdignity <emaiwoolabipupo@gmail.com>
2025-09-09 11:05:20 +0100

    django_project

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 # relationship_app/urls.py
 from django.urls import path
 from .views import list_books, LibraryDetailView   # ✅ import both FBV and CBV
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),  # function-based view
@@ -14,7 +16,14 @@ from .views import list_books, LibraryDetailView, register_view, login_view, log
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+     # Registration (function-based view)
+    path("register/", views.register, name="register"),
+
+    # Login (class-based built-in view with template)
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+
+    # Logout (class-based built-in view with template)
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
+
+
